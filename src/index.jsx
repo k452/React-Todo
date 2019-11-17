0ee0d2416c8008f16c63fdc36cea75c6ea470adf
vagrant <vagrant@localhost.localdomain>
2019-11-17 11:08:33 +0000

    second

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
+import Form from "./Form";
 
 render(
-    <App />,
-  document.getElementById("root")
+    <Form />,
+    document.getElementById("root")
 );
